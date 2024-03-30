02e22ffb6b80fa44f9f885a7ff70c18005a1fd6d
ooosj <tmdwn8775@gmail.com>
2024-03-31 00:28:26 +0900

    20240330

---
+++ week-2/osj/README.md
@@ -0,0 +1,22 @@
+## 20055 - 컨베이어 벨트 위의 로봇
+
+골드 5
+
+[문제 링크](https://www.acmicpc.net/problem/20055)
+
+[풀이](https://github.com/ooosj/Coding-test-study/blob/main/week-2/osj/%5Bboj%5D%2020055.py)
+
+### 핵심 아이디어
+
+로봇과 벨트를 다른 리스트를 이용하여 구현한다.
+
+<br>
+
+리스트의 회전은 rotate() 이용하여 구현하고
+
+로봇이 한 칸 움직이는 과정과 로봇을 올리는 과정을 나눠서 처리한다.
+
+<br>
+
+리스트를 회전해야한다면 rotate 함수를 잘 이용해보자
+
---
+++ week-3/osj/[boj] 7576.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+m, n = map(int,input().split())
+
+array = [list(map(int, input().split())) for _ in range(n)]
+
+queue = deque([])
+
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+result = 0
+
+for i in range(n):
+    for j in range(m):
+        if array[i][j] == 1:
+            queue.append([i,j])
+
+def bfs(array):
+    count = 0
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and array[nx][ny] == 0:
+                array[nx][ny] = array[x][y] + 1
+                queue.append([nx,ny])
+
+
+    return count
+
+bfs(array)
+
+for i in array:
+    for j in i:
+        if j == 0:
+            print(-1)
+            exit(0)
+    result = max(result, max(i))
+
+print(result - 1)

8d2db3de3416f1b07b32e773ab6046b2e583ff19
ooosj <tmdwn8775@gmail.com>
2024-04-06 16:04:58 +0900

    20240406

--- week-4/osj/[boj] 2260.py
+++ week-4/osj/[boj] 2260.py
@@ -3,13 +3,13 @@ from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-graph = []
+array = []
 
 visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
 visited[0][0][0] = 1
 
-for i in range(n):
-    graph.append(list(map(int, input())))
+for _ in range(n):
+    array.append(list(map(int, input().rstrip())))
 
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
@@ -25,14 +25,13 @@ def bfs(x, y, z):
         for i in range(4):
             nx = a + dx[i]
             ny = b + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 1 and c == 0 :
-                visited[nx][ny][1] = visited[a][b][0] + 1
-                queue.append((nx, ny, 1))
-            elif graph[nx][ny] == 0 and visited[nx][ny][c] == 0:
-                visited[nx][ny][c] = visited[a][b][c] + 1
-                queue.append((nx, ny, c))
+            if 0 <= nx < n and 0 <= ny < m:
+                if array[nx][ny] == 1 and c == 0 :
+                    visited[nx][ny][1] = visited[a][b][0] + 1
+                    queue.append((nx, ny, 1))
+                elif array[nx][ny] == 0 and visited[nx][ny][c] == 0:
+                    visited[nx][ny][c] = visited[a][b][c] + 1
+                    queue.append((nx, ny, c))
     return -1
 
 print(bfs(0, 0, 0))
